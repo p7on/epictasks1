aa1a558753acbcf4296c2fb4e3caf1402104316a
Semyon Pirogov <semyonpirogov@MacBook-Air-Semyon.local>
2019-04-05 00:23:43 +0300

    test

---
+++ call_tel.py
@@ -0,0 +1,10 @@
+x = int(input('code: '))
+y = int(input('duration: '))
+if x == 343:
+    print (y * 15)
+elif x == 381:
+    print (y * 18)
+elif x == 473:
+    print (y * 13)
+elif x == 485:
+    print (y * 11)
---
+++ fun1.py
@@ -0,0 +1,5 @@
+x = float(input())
+if 5.7 >= x >= -2.4:
+    print(round(x**2))
+else:
+    print(4)
