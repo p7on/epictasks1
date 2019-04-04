a1b68c5813e912b7a5aed70d3ee358a1ecdcae16
Semyon Pirogov <semyonpirogov@MacBook-Air-Semyon.local>
2019-04-04 23:48:08 +0300

    test

---
+++ table.py
@@ -0,0 +1,11 @@
+x = int(input('Element number:'))
+if x == 3:
+    print('Li')
+elif x == 25:
+    print('Mn')
+elif x == 17:
+    print('Cl')
+elif x == 80:
+    print('Hg')
+else:
+    print('Try something else')
