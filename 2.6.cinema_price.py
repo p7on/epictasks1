cd846fca269bef6b089fd0d2130cdc8f31e883e3
Semyon Pirogov <semyonpirogov@MacBook-Air-Semyon.local>
2019-04-05 14:52:05 +0300

    test

--- 2.6.cinema_price.py
+++ 2.6.cinema_price.py
@@ -1,15 +1,19 @@
 '''
 not ready
 '''
-
 film = input('Select movie: ')
 date = input('Today or tommorow?: ')
 time = int(input('Choose time: '))
 tickets = int(input('Quanity of tickets: '))
 price = int(input('Select price: ')) * tickets
+
 print('\nFilm: ', film, '\nDay: ', date, '\nTime: ', time, '\nQuanity of tickets: ', tickets)
-if date == 'Tommorow':
-    price *= 0.95
-if tickets >= 20:
-    price *= 0.8
-print(price)
+
+if film == 'Пятница' or film == 'Чемпионы' or film == 'Пернатая банда':
+    if date == 'Tommorow':
+        price *= 0.95
+    elif tickets >= 20:
+        price *= 0.8
+    print(price)
+else:
+    print('Error')
