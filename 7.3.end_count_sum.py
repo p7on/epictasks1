c492755df89675880077378fbdbd8685a3957234
Semyon Pirogov <semyonpirogov@MacBook-Air-Semyon.local>
2019-04-11 19:43:31 +0300

    7

--- 7.3.end_count_sum.py
+++ 7.3.end_count_sum.py
@@ -1,10 +1,13 @@
 result = 0
 while True:
-    n = input('Enter the number or stop to exit: ')
-    if n == 'stop':
-        break
-    else:
-        result += int(n)
+    try:
+        n = input('Enter the number or stop to exit: ')
+        if n == 'stop':
+            break
+        elif int(n) > 0:
+            result += int(n)
+    except ValueError:
+        print('Error')
 print(result)
 
-#Как сделать так чтобы выход по ключу был вне зависимости от его регистра (верхний/нижний)? Помимо доп. условия в if
+# Как сделать так чтобы выход по ключу был вне зависимости от его регистра (верхний/нижний)? Помимо доп. условия в if
