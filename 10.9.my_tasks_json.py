50cadd08552621a3421a62ff33637ce3fbfe09a4
Semyon Pirogov <semyonpirogov@MacBook-Air-Semyon.local>
2019-05-05 17:54:34 +0300

    fix

--- 10.9.my_tasks_json.py
+++ 10.9.my_tasks_json.py
@@ -2,6 +2,13 @@ import json
 
 TODO = []
 
+
+with open('data.json') as data_json:
+    TODO = json.load(data_json)
+    data_json.close()
+print(TODO)
+
+
 while True:
     welcome_text = 'Simple TODO:\n 1 to Add task\n 2 to show task list\n 3 for exit'
     print(welcome_text)
@@ -15,9 +22,11 @@ while True:
     elif key == 2:
         for task in TODO:
             print('Task name: ' + task['Task_name'], 'Category: ' + task['Category'], 'Time: ' + task['Time'], sep='\n')
+        print(TODO)
     elif key == 3:
-        with open('data.json', 'a') as out_file:
+        with open('data.json', 'w') as out_file:
             json.dump(TODO, out_file)
+            out_file.close()
         print('Saved!')
         break
     else:
