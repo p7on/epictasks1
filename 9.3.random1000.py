e51f5274adbcedfbf59c1f92b1010aa02188e760
Semyon Pirogov <semyonpirogov@MacBook-Air-Semyon.local>
2019-05-12 19:28:22 +0300

    pew

---
+++ 9.3.random1000.py
@@ -0,0 +1,17 @@
+import random
+
+
+def search_number(some_numbers):
+    counter = 0
+    min_index = some_numbers.index(min(some_numbers))
+    max_index = some_numbers.index(max(some_numbers))
+    for negative_number in some_numbers[min_index: max_index]:
+        if negative_number < 0:
+            counter += 1
+        else:
+            continue
+    return counter
+
+
+some_numbers = [random.randrange(-1000, 1000) for number in range(1000)]
+print(search_number(some_numbers))
